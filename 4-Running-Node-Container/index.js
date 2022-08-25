04d67e4ee70101a003aad6acedf1ebfcc35d33db
Lucas Primo <lucasprimo375@hotmail.com>
2022-08-25 09:41:32 -0300

    Handling SIGINT and SIGTERM inside node application running in node container

--- 4-Running-Node-Container/index.js
+++ 4-Running-Node-Container/index.js
@@ -1,4 +1,16 @@
 const express = require('express')
+const process = require('process')
+
+process.on('SIGINT', () => {
+    console.log('Hello there! Application is being interrupted!')
+    process.exit(0)
+})
+
+process.on('SIGTERM', () => {
+    console.log('Hello there! Application is being terminated!')
+    process.exit(0)
+})
+
 const app = express()
 
 app.get('/', function (req, res) {
